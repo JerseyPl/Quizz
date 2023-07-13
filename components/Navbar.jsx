a9dae231349b1fc820e0209ff438d66115f6962b
JerseyPl <Delr1602@gmail.com>
2023-07-13 19:06:55 +0300

    fix

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,9 +4,10 @@ module.exports = function Navbar({ user }) {
   return (
     <nav>
       <div class="nav-wrapper">
-        <a href="https://png.pngtree.com/png-vector/20210502/ourmid/pngtree-quiz-png-transparent-background-png-image_3257718.jpg" class="brand-logo">
+        <div>QUIZ</div>
+        {/* <img src="https://png.pngtree.com/png-vector/20210502/ourmid/pngtree-quiz-png-transparent-background-png-image_3257718.jpg" class="brand-logo">
           {user}
-        </a>
+        </img> */}
         <ul id="nav-mobile" class="right hide-on-med-and-down">
           <li>
             <a href="/themes">Темы</a>
