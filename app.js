ae95d6f83b79eea2a69af1399b91afcf79caeb04
JerseyPl <Delr1602@gmail.com>
2023-07-13 16:49:58 +0300

    Layout

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 require("@babel/register");
 const morgan = require("morgan");
 const express = require("express");
-const ssr = require("./middleware/renderComponent");
 const ReactDOMServer = require("react-dom/server");
 const React = require("react");
+const ssr = require("./middleware/renderComponent");
 const app = express();
 const PORT = 3000;
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(ssr);
-
 
 app.get("/", (req, res) => {
   res.send("hello");
