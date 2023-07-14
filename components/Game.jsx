0a0a795dcc83a9328630882f7fb536fbe9645fb5
JerseyPl <Delr1602@gmail.com>
2023-07-14 10:08:13 +0300

    GameValue

--- components/Game.jsx
+++ components/Game.jsx
@@ -1,13 +1,19 @@
-const React = require('react');
-const Layout = require('./Layout');
-const Navbar = require('./Navbar');
+const React = require("react");
+const Layout = require("./Layout");
+const Navbar = require("./Navbar");
 
-module.exports = function Game({title}) {
+module.exports = function Game({ title }) {
   return (
     <div>
       <Layout title={title}>
         <Navbar />
-        <div className="Game">Game</div>
+        <div className="Game">Вопрос:</div>
+        <button className="nextQuest">Следующий вопрос</button>
+        <form action="/" method="GET">
+          <input name="answer" placeholder="Ваш ответ" />
+          <button className="youAnswer">Ответить</button>
+        </form>
+        <h3>Ваш ответ правильный или нет</h3>
       </Layout>
     </div>
   );
