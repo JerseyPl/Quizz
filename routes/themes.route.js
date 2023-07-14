7d2f3a98397a3956dd7afde00653af087554922e
JerseyPl <Delr1602@gmail.com>
2023-07-14 13:15:05 +0300

    else

--- routes/themes.route.js
+++ routes/themes.route.js
@@ -1,16 +1,16 @@
-const router = require('express').Router();
-const ThemesList = require('../components/ThemesList');
-const { Theme, Question } = require('../db/models');
-const Game = require('../components/Game');
+const router = require("express").Router();
+const ThemesList = require("../components/ThemesList");
+const { Theme, Question } = require("../db/models");
+const Game = require("../components/Game");
 
-router.get('/', async (req, res) => {
-  console.log(1)
+router.get("/", async (req, res) => {
+  console.log(1);
   const theme = await Theme.findAll();
   // console.log(theme);
-  res.renderComponent(ThemesList, { title: 'Themes', theme });
+  res.renderComponent(ThemesList, { title: "Themes", theme });
 });
-router.get('/:themeId/question/:questionIndex', async (req, res) => {
-  console.log(2)
+router.get("/:themeId/question/:questionIndex", async (req, res) => {
+  console.log(2);
   // console.log(req.params);
   const { themeId, questionIndex } = req.params;
   const quest = await Question.findAll({ where: { themeId } });
@@ -18,10 +18,12 @@ router.get('/:themeId/question/:questionIndex', async (req, res) => {
   // console.log(index);
   if (index <= quest.length) {
     res.renderComponent(Game, {
-      title: 'Quest',
+      title: "Quest",
       index,
       question: quest[index - 1],
     });
+  } else {
+    res.redirect("/themes");
   }
 });
 module.exports = router;
